4e46ef69427cca59afbd5bc7f5f0a32133d04fe8
JackDesBwa <jackdesbwa@gmail.com>
2015-05-01 18:30:22 +0200

    board_install: update autorun with 4 panes in tmux

--- board_install/sikula_install_005_autorun.sh
+++ board_install/sikula_install_005_autorun.sh
@@ -30,22 +30,25 @@ autorum_dir=/root/autorun/
 
 cd \$autorum_dir
 
-tmux new-session -s "\$tmux_session" -d \$autorum_dir/panel_left.sh
+tmux new-session -s "\$tmux_session" -d \$autorum_dir/panel_topleft.sh
 tmux split-window -t "\$tmux_session" -h \$autorum_dir/panel_topright.sh
 tmux split-window -t "\$tmux_session" -v \$autorum_dir/panel_bottomright.sh
+tmux select-pane -t "\$tmux_session" -L
+tmux split-window -t "\$tmux_session" -v \$autorum_dir/panel_bottomleft.sh
 EOF
 
 chmod +x /root/autorun/autorun.sh
 
-cat << EOF > /root/autorun/panel_left.sh
+cat << EOF > /root/autorun/panel_topleft.sh
 #!/bin/bash
 echo \$0
 /bin/bash # Start interactive shell, otherwise the panel closes
 EOF
 
-chmod +x /root/autorun/panel_left.sh
+chmod +x /root/autorun/panel_topleft.sh
 
-cp /root/autorun/panel_left.sh /root/autorun/panel_topright.sh
-cp /root/autorun/panel_left.sh /root/autorun/panel_bottomright.sh
+cp /root/autorun/panel_topleft.sh /root/autorun/panel_topright.sh
+cp /root/autorun/panel_topleft.sh /root/autorun/panel_bottomright.sh
+cp /root/autorun/panel_topleft.sh /root/autorun/panel_bottomleft.sh
 
 echo "Connect to autorun scripts with ** sudo tmux attach -t sikula **" >> /etc/motd
