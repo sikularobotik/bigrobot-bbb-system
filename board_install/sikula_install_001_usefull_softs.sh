5f28a429d7bffe694a58a04bdc357a26c5525d9e
JackDesBwa <jackdesbwa@gmail.com>
2015-11-22 22:51:33 +0100

    board_install: add alsa-utils

--- board_install/sikula_install_001_usefull_softs.sh
+++ board_install/sikula_install_001_usefull_softs.sh
@@ -5,4 +5,4 @@ SIKULA_INSTALL_SCRIPT=1 ./sikula_install.sh || exit 1
 # ========================
 
 pacman -Sy
-pacman -S vim socat i2c-tools screen gnu-netcat python3
+pacman -S vim socat i2c-tools screen gnu-netcat python3 alsa-utils
